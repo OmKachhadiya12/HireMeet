0ec25b47bb06c7fd4536ee1868b92c72962ef610
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-07-02 17:03:35 +0530

    Add the Inggest function but remember have to add the Backend URL.

--- server/src/lib/env.js
+++ server/src/lib/env.js
@@ -6,5 +6,10 @@ export const ENV = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     MONGODB_URL: process.env.MONGODB_URL,
-
+    CLIENT_URL: process.env.CLIENT_URL,
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+    
 }
