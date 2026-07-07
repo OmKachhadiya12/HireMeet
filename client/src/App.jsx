1a487c7ca09be2e21650c42288d9fcbd558b0966
om kachhadiya <kachhadiyaom2005@gmail.com>
2026-07-07 12:16:53 +0530

    Add the frontend tools.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,26 @@
 import { useState } from 'react'
-import './App.css'
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from "@clerk/clerk-react";
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser, } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import ProblemsPage from "./pages/ProblemsPage.jsx";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+
+  const { isSignedIn } = useUser();
+
   const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Welcome to the app</h1>
 
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Login</button>
-        </SignInButton>
-      </SignedOut>
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+      </Routes>
 
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn>
-
-      <UserButton />
+      <Toaster toastOptions={{ duration: 3000 }} />
     </>
   )
 }
